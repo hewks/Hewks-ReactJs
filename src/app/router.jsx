47b5104b699be79a489d855397919f9089f58e55
CtrlProgrammer <sebastianto1999@gmail.com>
2020-01-22 20:30:24 -0500

    Add responsive style to navigation

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -12,9 +12,23 @@ export default class Router extends React.Component {
   constructor(props) {
     super(props);
 
+    this.leftNav = React.createRef();
+
+    this.toggleLeftNav = this.toggleLeftNav.bind(this);
+
     this.state = {
       lang: "es"
     };
+
+    this.leftNavigation = {
+      active: "hw-active-left-navigation"
+    };
+  }
+
+  toggleLeftNav() {
+    this.leftNav.current.classList.contains(this.leftNavigation.active)
+      ? this.leftNav.current.classList.remove(this.leftNavigation.active)
+      : this.leftNav.current.classList.add(this.leftNavigation.active);
   }
 
   render() {
@@ -25,6 +39,9 @@ export default class Router extends React.Component {
           <div className="hw-navigation">
             <div className="hw-top-navigation">
               <Link to="/">Hewks</Link>
+              <div className="hw-toggler" onClick={this.toggleLeftNav}>
+                <span className="fas fa-bars"></span>
+              </div>
               <div className="hw-links">
                 <Link to="/">Inicio</Link>
                 <Link to="/servicios">Servicios</Link>
@@ -47,6 +64,29 @@ export default class Router extends React.Component {
                     <span className="fab fa-instagram"></span>
                   </a>
                 </div>
+              </div>
+            </div>
+            <div className="hw-left-navigation" ref={this.leftNav}>
+              <Link to="/">Inicio</Link>
+              <Link to="/servicios">Servicios</Link>
+              <Link to="/protafolio">Portafolio</Link>
+              <Link to="/proyectos">Proyectos</Link>
+              <Link to="/contacto">Contacto</Link>
+              <div className="hw-social">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.github.com/ctrlprogrammer"
+                >
+                  <span className="fab fa-github"></span>
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.instagram.com/hewks_net"
+                >
+                  <span className="fab fa-instagram"></span>
+                </a>
               </div>
             </div>
           </div>
