2c066989b7cae688c55979ddac329111fb55855d
CtrlProgrammer <sebastianto1999@gmail.com>
2020-02-07 18:51:28 -0500

    Inlcude lang on wrapper

--- src/app/comp/wrapper.jsx
+++ src/app/comp/wrapper.jsx
@@ -4,13 +4,13 @@ import "./../../assets/css/comp/wrapper.css";
 
 export class Big extends React.Component {
   componentDidMount() {
-    if (this.props.iconsAnim === true) {
+    if (this.props.wrapper.anim === true) {
       const allIcons = document.querySelectorAll(".hw-wrapper-icon");
       setTimeout(() => {
         allIcons.forEach(icon => {
           icon.classList.add("hw-icon-move");
         });
-      }, 2000);
+      }, 4000);
     }
   }
 
@@ -19,13 +19,13 @@ export class Big extends React.Component {
       <div
         className="hw-wrapper hw-big-wrapper"
         style={{
-          backgroundImage: "url(" + this.props.image + ")"
+          backgroundImage: "url(" + this.props.wrapper.image + ")"
         }}
         ref={this.wrapper}
       >
         <div className="hw-wrapper-info">
-          <h4>{this.props.title}</h4>
-          <p>{this.props.text}</p>
+          <h4>{this.props.wrapper.title}</h4>
+          <p>{this.props.wrapper.text}</p>
         </div>
         <div className="hw-icons-container">
           <span className="hw-wrapper-icon fab fa-react"></span>
--- src/app/pages/main.jsx
+++ src/app/pages/main.jsx
@@ -5,27 +5,15 @@ import { MiniServices } from "./../comp/services.jsx";
 
 import "./../../assets/css/pages.css";
 
-import wrapperBackground from "./../../assets/img/1.jpg";
-
 export default class Main extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      wrapperTitle: "Hewks",
-      wrapperText:
-        "Queremos que tu empresa crezca como nunca antes, por esto te ofrecemos las mejores herramientas de este siglo."
-    };
-
-    this.icon = ["fab fa-github"];
-  }
-
   render() {
+    var wrapper = {};
     var services = [];
 
     switch (this.props.lang) {
       case "es":
         services = require("../../assets/lang/es/services/mini.jsx").default;
+        wrapper = require("../../assets/lang/es/wrapper.jsx").default;
         break;
       case "en":
         services = require("../../assets/lang/es/services/mini.jsx").default;
@@ -37,13 +25,7 @@ export default class Main extends React.Component {
 
     return (
       <div className="hw-page">
-        <Big
-          image={wrapperBackground}
-          title={this.state.wrapperTitle}
-          text={this.state.wrapperText}
-          iconsAnim={true}
-          icons={this.icons}
-        />
+        <Big wrapper={wrapper} />
         {/** Photo by Zan on Unsplash */}
         <MiniServices services={services} />
       </div>
